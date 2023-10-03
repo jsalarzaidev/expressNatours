a4ae0ee6e318bf1e842514a0ef36a8bab90ef227
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-10-03 10:03:12 +0500

    API handling GET request

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  // can accept couple of arguments
-  // send some data back to server very quickly
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this URL');
+// app.get('/', (req, res) => {
+//   // can accept couple of arguments
+//   // send some data back to server very quickly
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('You can post to this URL')
+// })
+
+// only executed once.
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 // starting up a server
