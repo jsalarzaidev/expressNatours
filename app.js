8c1eab99c48c71240e7ad3988cfc84b2bb0a05e7
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-10-03 12:19:14 +0500

    handling PATCH requests

--- app.js
+++ app.js
@@ -85,6 +85,22 @@ app.post('/api/v1/tours', (req, res) => {
   //res.send('Done'); // always need to send something in order to finish request response cycle.
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 // starting up a server
 
 // adding port to listen
