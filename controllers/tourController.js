a55dfffd5cc6a3f567780196a9e9842002fe081b
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-10-18 17:37:49 +0500

    Refactoring API Features: limitFields and Paginate

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,9 +28,9 @@ class APIFeatures {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     //console.log(JSON.parse(queryStr));
 
-    this.query.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
     // let query = Tour.find(JSON.parse(queryStr)); // this method will return a promise
-    return this;
+    return this; // `this` is the entire object
   }
 
   sort() {
@@ -42,6 +42,26 @@ class APIFeatures {
     } else {
       this.query = this.query.sort('-createdAt'); // decending order
     }
+    return this; // `this` is the entire object
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields); //projecting
+    } else {
+      this.query = this.query.select('-__v'); //excluding "__v: 0" mongoose uses this
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1; // nice trick: to convert string to number, and default to page 1
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=10 -> user is at page # 2 and shows 10 results per page
+    // page 1: 1->10, page 2: 11->20, page 3: 21->30 ....
+    this.query = this.query.skip(skip).limit(limit);
     return this;
   }
 }
@@ -52,56 +72,14 @@ class APIFeatures {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-    // 1A) Filtering
-    console.log(req.query);
-    // const queryObj = { ...req.query };
-    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    // // remove all the fields from queryObj
-    // excludedFields.forEach((el) => delete queryObj[el]);
-
-    // // 1B) Advanced Filtering
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // //console.log(JSON.parse(queryStr));
-
-    // let query = Tour.find(JSON.parse(queryStr)); // this method will return a promise
-
-    // 2) Sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   //console.log(sortBy);
-    //   query = query.sort(sortBy);
-    //   // sort('price ratingsAverage')
-    // } else {
-    //   query = query.sort('-createdAt'); // decending order
-    // }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields); //projecting
-    } else {
-      query = query.select('-__v'); //excluding "__v: 0" mongoose uses this
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1; // nice trick: to convert string to number, and default to page 1
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // page=2&limit=10 -> user is at page # 2 and shows 10 results per page
-    // page 1: 1->10, page 2: 11->20, page 3: 21->30 ....
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     //EXECUTE QUERY
     // goal: chain these methods one afte another.
 
-    const features = new APIFeatures(Tour.find(), req.query).filter().sort();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
     const tours = await features.query;
 
     res.status(200).json({
