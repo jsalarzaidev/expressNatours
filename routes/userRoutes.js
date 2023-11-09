7d69b7e45b1d3ace65f0163ce2be1f04fab0a3f1
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-11-09 22:52:08 +0500

    Creating New Users and Managing Passwords

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 /*
  * Route Handlers for Users
  */
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
