fbe0186e7e4d6000cd2043ca7ba9c31c6d78323a
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-11-12 18:34:23 +0500

    Loggin in Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 const catchAsync = require('./../utils/catchAsync');
 const User = require('./../models/userModel');
+const AppError = require('./../utils/appError');
 
+// sign up
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -26,5 +28,29 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+// logIn
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exists
+  if (!email || !password) {
+    return next(new AppError('Please Provide email and Password', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({
+    email, // email: email
+  }).select('+password');
+  // 3) if everything is ok, send token to client
+
+  console.log(user);
+
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
